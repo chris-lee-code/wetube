e9745c0a586d1af20498220affffa90107fd197f
Christopher Lee <christopherlee0222@users.noreply.github.com>
2020-04-07 21:54:51 +0800

    updated April 7th

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv/types";
+import dotenv from "dotenv";
 
 dotenv.config();
 
--- init.js
+++ init.js
@@ -1,6 +1,6 @@
 import "./db";
 import app from "./app";
-import dotenv from "dotenv/types";
+import dotenv from "dotenv";
 dotenv.config();
 import "./models/Video";
 import "./models/Comment";
